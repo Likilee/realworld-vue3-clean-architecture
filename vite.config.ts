52ccedf77bcd55cf1343c67389cec20a47ea64dd
Likilee <gnsdlrl@daum.net>
2023-06-12 22:12:06 +0900

    chore: remove autoimport

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      dts: 'src/auto-imports.d.ts',
-      dirs: ['src/shared-kernel/**']
-    })
-  ]
+  }
 })
